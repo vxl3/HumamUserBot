28f68f67c4d93f9d7384b42f1c3a8e0d9b2d7884
vxl3 <wh2003w0@gmail.com>
2025-08-07 15:18:01 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+api_id = 26619718  # عوّض هذا الرقم بالـ API_ID مالك
+api_hash = "a7bc8e1682fed63c806f5399fa71dbf7"  # حط هنا الـ API_HASH مالك
+session_name = "humam"  # اسم الجلسة، خليه أي شي (يفضل اسم ثابت)
